9e2ad222bb1b3afe31d81f10e5026832af45deaa
John-Mary-M <johnmarluxin8@gmail.com>
2024-03-27 04:51:22 +0300

    generated HTML from a loop and rendered it to the page

--- scripts/11-to-do-list.js
+++ scripts/11-to-do-list.js
@@ -7,7 +7,27 @@
 */
 
 // empty Array to store todos
-const todoList = [];
+const todoList = [
+  "make dinner",
+  "wash dishes"
+];
+
+renderTodoList();
+// generating the HTML
+function renderTodoList(){
+  let todoListHTML = '';
+
+  for (i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`
+    todoListHTML += html
+  }
+  // console.log(todoListHTML);
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
+
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -18,4 +38,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = '';
+
+  renderTodoList();
 }
