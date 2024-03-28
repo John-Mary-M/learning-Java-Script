1c1ab704819dfbd43fbc4708edfcb5731ac751b3
John-Mary-M <johnmarluxin8@gmail.com>
2024-03-28 04:34:26 +0300

    continuing with exercise 11f

--- exercises/scripts/11a.js
+++ exercises/scripts/11a.js
@@ -29,7 +29,7 @@ function arraySwap(array){
 }
 console.log(arraySwap(arry));
 console.log(arraySwap(nums));
-
+/*
 // d loop that counts 0 -> 10 in increaments of 2
 for (i = 0; i < 11; i+=2){
   console.log(i);
@@ -39,3 +39,37 @@ for (i = 0; i < 11; i+=2){
 for (i = 5; i !== 0; i--){
   console.log(i);
 }
+*/
+// 11f repeating d and e above with a while loop
+let count = 0;
+while (count < 11){
+  console.log(count);
+  count += 2;
+}
+let countDown = 5;
+while (countDown !== 0){
+  console.log(countDown);
+  countDown -= 1;
+}
+
+// 11g looping through an array and increamenting each value by 1
+const numArry = [1, 2, 3];
+const newArry = []; // newArry will hold the incremented values
+for (let i = 0; i < numArry.length; i++){
+  const value = numArry[i]; // storing each value of numArry in a variable
+  newArry.push(value + 1); // adding the values to newArry one by one.
+}
+console.log(newArry);
+
+// 11h. addOne(array) returns an array where each value is increased by one
+function addOne(array){
+  const returnArray = [];
+  for (let i = 0; i < array.length; i++){
+    const members = array[i];
+    returnArray.push(members + 1);
+  }
+  return returnArray;
+}
+
+console.log(addOne([1, 2, 3]));
+console.log(addOne([-2, -1, 0, 99]));
